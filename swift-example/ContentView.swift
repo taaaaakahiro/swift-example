bc54c70154b1ff23f24077e41a251b9292f05b42
takahiro <takapi854@gmail.com>
2024-01-07 18:17:17 +0800

    Initial Commit

---
+++ swift-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-example
+//
+//  Created by 小出隆太 on 2024/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-example/swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_exampleApp.swift
+//  swift-example
+//
+//  Created by 小出隆太 on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
